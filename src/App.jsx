81ce714f76e3c3b3dc37a8652477968dc6c13ab1
Hedita <hedi.takrami@gmail.com>
2023-03-06 12:12:36 +0330

    import component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import Tabs from "./Tabs/Tabs";
+
+const App = () => {
+  return <Tabs />;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
