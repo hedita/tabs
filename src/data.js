a3e0865d26055a1ce56b8904391c1fcb8c732ae2
Hedita <hedi.takrami@gmail.com>
2023-03-06 12:20:47 +0330

    add data file

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    id: "1",
+    title: "Code",
+    icon: "code",
+    content:
+      "Lorem Ipsum on lihtsalt proovitekst, mida kasutatakse printimis- ja ladumistööstuses. See on olnud tööstuse põhiline proovitekst juba alates 1500. aastatest, mil tundmatu printija võttis hulga suvalist teksti, et teha trükinäidist. Lorem Ipsum ei ole ainult viis sajandit säilinud, vaid on ka edasi kandunud elektroonilisse trükiladumisse, jäädes sealjuures peaaegu muutumatuks. See sai tuntuks 1960. aastatel Letraset'i lehtede väljalaskmisega, ja hiljuti tekstiredaktoritega nagu Aldus PageMaker, mis sisaldavad erinevaid Lorem Ipsumi versioone.",
+  },
+  {
+    id: "2",
+    title: "About",
+    icon: "pen-to-square",
+    content:
+      "On teada-tuntud fakt, et lehe kujunduse vaatamisel juhib selle loetav sisu lugeja tähelepanu kõrvale. Lorem Ipsumi kasutamise mõte seisneb selles, et sellel on enam-vähem normaalne jaotus tähti, mitte nagu korduvatel sõnadel, nähes välja nagu tavaline loetav tekst. Paljud tekstiredaktorid ja veebilehtede valmistamise programmid kasutavad Lorem Ipsumit vaikimisi mudeltekstina, ja trükkides otsingusse  võib leida veebilehti, mida ei ole veel nende õige sisuga täidetud. Aastatega on tekkinud mitmeid versioone, mõnikord kogemata, mõnikord meelega (huumori sisestamisega jne).",
+  },
+  {
+    id: "3",
+    title: "Services",
+    icon: "chart-column",
+    content:
+      "Saadaval on mitmeid erinevaid Lorem Ipsumi variante, kuid enamik neist on läbi teinud muutusi kas sisestatud huumori või suvaliste sõnade tõttu, mis ei näe üldsegi usutavad välja. Kui te kavatsete Lorem Ipsumit kasutada, peate te kindel olema, et kuskil keset teksti midagi piinlikku ei ole. Kõik Lorem Ipsumi generaatorid kipuvad kindlaid lauseridu kordama, seega on see sait siin esimene tõeline Lorem Ipsumi generaator Internetis. See kasutab sõnastikku üle 200 ladina sõnaga ja, koos hulga lausestruktuuride mudelitega, toodab korralikku Lorem Ipsumit. Toodetud Lorem Ipsum on seega alati vaba kordustest, naljadest, ebaharilikest sõnadest jne.",
+  },
+  {
+    id: "4",
+    title: "Portfolio",
+    icon: "folder-open",
+    content:
+      "ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+  },
+  {
+    id: "5",
+    title: "Contact",
+    icon: "envelope",
+    content:
+      "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.",
+  },
+];
